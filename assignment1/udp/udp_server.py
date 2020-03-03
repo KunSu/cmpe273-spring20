b1fee37e2557bebfe6dbc9f05fee616b24d372f8
KunSu <kunsu.903@gmail.com>
2020-03-02 17:03:07 -0800

    update UDP draft version

--- assignment1/udp/udp_server.py
+++ assignment1/udp/udp_server.py
@@ -14,6 +14,7 @@ def listen_forever():
     sock.bind((UDP_IP, UDP_PORT))
     print('Server started at port 4000.')
 
+    count = 0
     while True:
         data, addr = sock.recvfrom(BUFFER_SIZE)
         if data:
