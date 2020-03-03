b05805b1d1db051ada5dd7f270f1262f4a0edbdb
KunSu <kunsu.903@gmail.com>
2020-03-03 00:19:13 -0800

    TCP Done

--- assignment1/tcp/tcp_client.py
+++ assignment1/tcp/tcp_client.py
@@ -1,35 +1,46 @@
-# Author: Kun Su
-# Code and Comment reference: https://docs.python.org/3/howto/sockets.html
 import socket
-
+import time
+import argparse
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5000
 BUFFER_SIZE = 1024
+MESSAGE = "ping"
 
-def listen_forever():
-    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # use TCP_IP = 'localhost' or '127.0.0.1' which only visible within the same machine
-    # use TCP_IP = socket.gethostname() will be visible to the outside world.
-    serversocket.bind((TCP_IP, TCP_PORT)) 
-    serversocket.listen(5) # The number of max connection 
 
-    # 3 general ways to handle socket
-    # 1. dispatching a thread to handle clientsocket
-    # 2. create a new process to handle clientsocket
-    # 3. restructure this app to use non-blocking sockets
-    while True:
-        # accept connections from outside
-        (clientsocket, address) = serversocket.accept() 
-        # The purpose of "server" socket is to produces “client” sockets
+def send(args):
+    id = args[0]
+    delay = args[1]
+    number = args[2]
 
-        if clientsocket:
-            print(f'Connection address:{address}')
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((TCP_IP, TCP_PORT))
 
-            data = clientsocket.recv(BUFFER_SIZE)
-            print(f"received data: {data.decode()}")
-            clientsocket.send("pong".encode())
+    for count in range(number):
+        time.sleep(delay)
+        print("Sending data:", MESSAGE)
+        s.send(f"{id}:{MESSAGE}".encode())
+        data = s.recv(BUFFER_SIZE)
+        print("Recevied data:", data.decode())
 
-    clientsocket.close()
+    s.close()
 
-listen_forever()
+
+def get_client_id():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--id', type=str, help="[client id]")
+    parser.add_argument('--delay', default=1, type=int,
+                        help="[delay in seconds between messages]")
+    parser.add_argument('--number', default=60, type=int,
+                        help="[number of 'ping' messages]")
+
+    args = parser.parse_args()
+    id = args.id
+    delay = args.delay
+    number = args.number
+
+    return id, delay, number
+
+
+send(get_client_id())
